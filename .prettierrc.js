b6b86ad73400cf34e29acaf4ad937f20819d8360
Nicklas Wei <shiwoen@gmail.com>
2020-01-16 15:53:13 +0100

    Adding additional prettier rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  singleQuote: true
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingCommas: 'none',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf'
 };
